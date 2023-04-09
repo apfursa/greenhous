e55370f0e9f5e7d8add5fd8c2dda9916113979ca
apfursa <apfursa@gmail.com>
2023-04-09 10:44:48 +0300

    исправлено inactiv на inactive

--- views/setup/temperature.php
+++ views/setup/temperature.php
@@ -29,6 +29,7 @@ $time = strtotime($pokazaniyaObj->date);
 $max = $datchObj->max;
 $min = $datchObj->min;
 $noCallTime = strtotime($errorObj->dateStartCall);
+$date1 = date("Y-m-d", $noCallTime);
 ?>
 <p>Текущие показания: <?php echo $pokazaniyaObj->value ?></p>
 <p>Дата и время: <?php echo date("d.m.Y H:i:s", $time)?></p>
@@ -37,14 +38,37 @@ $noCallTime = strtotime($errorObj->dateStartCall);
     <input type="text" name="maxTemp" id="maxTemp" value="<?php echo $max ?>" size="2"><br><br>
     <label for="minTemp">Минимальное значение</label>
     <input type="text" name="minTemp" id="minTemp" value="<?php echo $min ?>" size="2"><br>
-    <label for="inactiv">Не звонить до:</label><br>
-    <?php echo date("d.m.Y H:i:s", $noCallTime)?><br><br>
-    <input type="date" id="inactiv" name="inactiv" size="1">
-    <input type="time" id="inactivTime" name="inactivTime" size="1"><br><br>
+    <label for="inactive">Не звонить до:</label><br>
+    <span id="date">
+        <?php echo $date1 ?>
+    </span>
+    <br><br>
+    <input type="date" id="inactive" name="inactive" value="" size="1">
+    <input type="time" id="inactiveTime" name="inactiveTime" size="1"><br><br>
     <input type="hidden" name="datchId" value="<?php echo $datchId ?>">
     <input type="submit" name="submit" value="Cохранить">
 </form>
 </body>
+<!--<script>-->
+<!--    // Получаем дату с сервера в формате YYYY-MM-DD-->
+<!--    var dateFromServer = "2021-10-01";-->
+<!---->
+<!--    // Находим поле ввода по id-->
+<!--    var dateField = document.getElementById("dateField");-->
+<!---->
+<!--    // Устанавливаем дату как значение по умолчанию для поля ввода-->
+<!--    dateField.value = dateFromServer;-->
+<!--</script>-->
 
+<script>
+    // Получаем сегодняшнюю дату в формате YYYY-MM-DD
+    let date = document.getElementById('date')
+
+
+    // Устанавливаем значение в поле input
+    document.querySelector("#inactive").value = date;
+
+   //inactive.value = date;
+</script>
 
 </html>
